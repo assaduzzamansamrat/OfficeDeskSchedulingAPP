f0e90553869429d26d461e8e22eac6df777be026
assaduzzamansamrat <assaduzzamansamrat@gmail.com>
2022-01-26 00:07:19 +0600

    update

--- OfficeDeskSchedulingApplication/OfficeDeskSchedulingApplication/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace OfficeDeskSchedulingApplication.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
