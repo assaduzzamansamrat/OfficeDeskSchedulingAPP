ecbf410259e83683cbb5946b9198ffb3d9aff8ce
assaduzzamansamrat <assaduzzamansamrat@gmail.com>
2022-01-25 17:57:13 +0600

    Update

---
+++ OfficeDeskSchedulingAPP/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Office_Desk_Scheduling_APP.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
