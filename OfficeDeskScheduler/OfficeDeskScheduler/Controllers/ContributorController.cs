f204291a30081d719082ec6d5dcab13d5c47e944
assaduzzamansamrat <assaduzzamansamrat@gmail.com>
2022-01-27 00:29:37 +0600

    Update

---
+++ OfficeDeskScheduler/OfficeDeskScheduler/Controllers/ContributorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OfficeDeskScheduler.Controllers
+{
+    public class ContributorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
