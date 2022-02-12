57f5452d369831c121fa239180e5fb2f885f4b44
assaduzzamansamrat <assaduzzamansamrat@gmail.com>
2022-02-12 22:43:11 +0600

    Update

---
+++ OfficeDeskScheduler/Services/EntityModels/DeskBookingDetails.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.EntityModels
+{
+    public class DeskBookingDetails
+    {
+        public long Id { get; set; }
+        public long DeskId { get; set; }
+        public string? DeskNumber { get; set; }
+        public DateTime? StartDateTime { get; set; }
+        public DateTime? EndDateTime { get; set; }
+        public long BookedBy { get; set; }
+        public long AssignedContributorId { get; set; }
+        public string? AssignedContributorName { get; set; }
+        public string? Map { get; set; }
+        public string? Location { get; set; }
+        public long TeamId { get; set; }
+    }
+}
---
+++ OfficeDeskScheduler/Services/EntityModels/TeamDetails.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.EntityModels
+{
+    public class TeamDetails
+    {
+
+       public long Id { get; set; }
+        public long ManagerId { get; set; }
+        public string? TeamName { get; set; }
+        public int TeamSize { get; set; }
+        public string? EquipmentDetails { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public long CreatedBy { get; set; }
+        public DateTime? EditedDate { get; set; }
+        public long EditedBy { get; set; }
+        public string ManagerName { get; set; }
+    }
+}
